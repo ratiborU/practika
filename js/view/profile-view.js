440a0fae0d7cec81c220960016eebf1adb0f44d7
ratiborU <tvinck_444@mail.ru>
2023-07-11 13:40:39 +0500

    профиль

--- js/view/profile-view.js
+++ js/view/profile-view.js
@@ -7,11 +7,11 @@ const createTemplate = (profile) => (
       <div class="profile__data">
         <div class="profile__first-name">
           <p class="profile__first-name-text profile__text">Имя</p>
-          <input class="profile__first-name-input profile__input" type="text" name="profile__first-name-input" placeholder="введите ваше имя..." value="${profile.firstName}">
+          <input class="profile__first-name-input profile__input" type="text" name="profile__first-name-input" placeholder="введите ваше имя..." value="${profile.name}">
         </div>
         <div class="profile__second-name">
           <p class="profile__second-name-text profile__text">Фамилия</p>
-          <input class="profile__second-name-input profile__input" type="text" name="profile__second-name-input" placeholder="введите вашу фамилию..." value="${profile.secondName}">
+          <input class="profile__second-name-input profile__input" type="text" name="profile__second-name-input" placeholder="введите вашу фамилию..." value="${profile.surname}">
         </div>
         <div class="profile__email">
           <p class="profile__email-text profile__text">Электронная почта</p>
@@ -29,7 +29,7 @@ export default class ArticleView {
   }
 
   getTemplate () {
-    this.profile = this.getProfileDate;
+    this.profile = this.getProfileDate();
     return createTemplate(this.profile);
   }
 
@@ -49,9 +49,13 @@ export default class ArticleView {
   // получение данных пользователя
   getProfileDate = () => {
     return {
-      firstName: "Иван",
-      secondName: "Иванов",
-      email: "ivan.ivanov@gmail.com"
+      name: "Иван",
+      surname: "Иванов",
+      email: "ivan.ivanov@gmail.com",
+      hashed_password: "123",
+      is_active: true,
+      is_superuser: false,
+      is_verified: false
     }
   }
 
